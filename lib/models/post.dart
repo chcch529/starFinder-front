87a4afca4eedddc403e0eb5b953f82170f18a64e
chaechae <juold0568@naver.com>
2025-05-31 23:17:54 +0900

    refactor: postBubble 형식에 맞게 model 수정

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -4,13 +4,19 @@ class Post {
   final String nickname;
   final String? profileUrl;
   final DateTime createdAt;
+  final int likeCnt;
+  final int commentCnt;
+  final int userId;
 
   Post({
     required this.id,
     required this.content,
     required this.nickname,
     required this.profileUrl,
-    required this.createdAt
+    required this.createdAt,
+    required this.likeCnt,
+    required this.commentCnt,
+    required this.userId
 
   });
 
@@ -21,6 +27,9 @@ class Post {
       nickname: json['nickname'] as String,
       profileUrl: json['profileUrl'] as String?,
       createdAt: DateTime.parse(json['createdAt']),
+      likeCnt: json['lickCnt'],
+      commentCnt: json['commentCnt'],
+      userId: json['userId'],
     );
   }
 }
